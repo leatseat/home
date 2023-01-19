ddaf84968216b89a5f4355f5f6d678699b2f2936
leatseat <123127212+leatseat@users.noreply.github.com>
2023-01-19 18:56:38 -0400

    s
    ss

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 
 fetch("https://api.ipdata.co/?api-key=c5f0c58519c44542c6f4119781a3f572488f269f5d12f907a3daa004")
 .then(x=> x.json())
-.then(x=> console.log(x))
+.then(x=> {console.log(x)
+          
+           setTimeout(alert(3),5000)
+          
+          })
+
